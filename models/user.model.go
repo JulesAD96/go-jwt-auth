752e5215ed40293f011fefb8791e6701e9eccd6f
Jules <adonsijules9@gmail.com>
2022-12-23 17:25:56 +0100

    DBResponse type

--- models/user.model.go
+++ models/user.model.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type SignUpInput struct {
 	Name            string    `json:"name" bson:"name" binding:"required"`
@@ -16,4 +20,15 @@ type SignUpInput struct {
 type SignInInput struct {
 	Email    string `json:"email" bson:"email" binding:"required"`
 	Password string `json:"password" bson:"password" binding:"required"`
+}
+
+type DBResponse struct {
+	ID              primitive.ObjectID `json:"id" bson:"_id"`
+	Name            string             `json:"name" bson:"name"`
+	Password        string             `json:"password" bson:"password"`
+	PasswordConfirm string             `json:"passwordConfirm,omitempty" bson:"passwordConfirm,omitempty"`
+	Role            string             `json:"role" bson:"role"`
+	Verified        bool               `json:"verified" bson:"verified"`
+	CreatedAt       time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt       time.Time          `json:"updated_at" bson:"updated_at"`
 }
