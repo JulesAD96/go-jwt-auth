1beb33a335d8fb29c08d28aac7163196e9200ed7
Jules <adonsijules9@gmail.com>
2022-12-29 12:39:16 +0100

    Bug fixing

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func initRouter() *gin.Engine {
 	api := router.Group("/api")
 	{
 		api.POST("/token", controllers.GenerateToken)
-		api.POST("/user/register", controllers.Register)
+		api.POST("/register", controllers.Register)
 		secured := api.Group("/secured").Use(middlewares.Auth())
 		{
 			secured.GET("/ping", controllers.Ping)
